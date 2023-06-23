1e56e620d5a280d79f9cfeb5e3b867717cda6d94
Jonathan Magnet <j.magnet@outlook.com>
2023-06-23 15:06:55 +0200

    generation des controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'missions/index'
+  get 'listing/index'
+  get 'listing/show'
+  get 'listing/create'
+  get 'listing/update'
+  get 'listing/delete'
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  ressources: listing only(index)
+
 end
