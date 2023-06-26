e6e5b179aafc3f2b0a200f4a5a2ae2d15b5382b3
Jonathan Magnet <j.magnet@outlook.com>
2023-06-26 10:22:20 +0200

    codage des controllers

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,16 +1,53 @@
 class ListingsController < ApplicationController
+  before_action :set_listing, only: [:show, :update, :destroy]
+
   def index
+    @listing = Listing.pluck(:id, :num_rooms)
+                      .map { |idi, num| { id: idi, num_rooms: num } }
+
+    @responce = { listings: @listing }
+    render json: @responce
+    # raise
   end
 
   def show
+            @response = {
+            id: @listing.id,
+            num_rooms: @listing.num_rooms
+        }
+
+        render json: @response
   end
 
   def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      render nothing: true, status: :created
+    else
+      render json: @listing.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @listing.update(listing_params)
+      render nothing: true, status: :updated
+    else
+      render json: @listing.errors, status: :unprocessable_entity
+    end
   end
 
-  def delete
+  def destroy
+    @listing.destroy
+    render nothing: true, status: :no_content
+  end
+
+  private
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(:num_rooms)
   end
 end
--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,4 +1,10 @@
 class MissionsController < ApplicationController
   def index
+    @mission = Mission.pluck(:listing_id, :mission_type, :date, :price)
+                      .map do |list, mission, date, price|
+                              {listing_id: list, mission_type: mission, date: date, price: price}
+
+    @responce = {missions: @mission}
+    render json: @responce
   end
 end
